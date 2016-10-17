476e6e1ab04217de7b68b27aa0f91051314a5585
Sébastien Lengagne <sebastien.lengagne@univ-bpclermont.fr>
2016-10-17 11:04:23 +0200

    modif de l'auto install

--- auto_install.sh
+++ auto_install.sh
@@ -4,21 +4,6 @@ sudo apt-get update
 
 sudo apt-get install cmake g++ libboost-all-dev libcv-dev libhighgui-dev libcvaux-dev libopencv-gpu-dev qt4-dev-tools
 
-mkdir auto_install 
-
-cd auto_install 
-
-git clone  https://github.com/lengagne/mogs_tinyxml.git
-cd mogs_tinyxml
-mkdir build
-cd build 
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make 
-sudo make install 
-
-cd ../../..
-pwd
-
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
